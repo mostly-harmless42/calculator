a190daaf2eab1738c6d57560d44d6a88609f4123
mostly-harmless42 <wrgread1@gmail.com>
2022-02-27 13:01:38 -0800

    Add logic

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function makeKey(str) {
   key.innerHTML = str;
   key.style.flex = '1 0 24%';
   keys.append(key);
-//  key.addEventListener("click", e => input(e.target));
+  key.addEventListener("click", e => input(e.target));
 };
 
 function makeKeyArray(arr) {
@@ -37,3 +37,95 @@ makeKeyArray(keyArray);
 
 document.getElementById("0").style.flex = '1 0 49.1%';
 
+
+function sum(a, b) {return a+b;};
+function dif(a, b) {return a-b;};
+function mul(a, b) {return a*b;};
+function div(a, b) {return a/b;};
+function pow(a, b) {return Math.pow(a, b);};
+function operate(op, a, b=0) {
+  a = Number(a);
+  b = Number(b);
+  return  Math.round(op(a, b)*pow(10, 12))/pow(10, 12).toString();
+};
+
+
+let n = 0;
+let op = 0;
+
+function input(key) {
+  d = disp.innerHTML;
+
+  if(d === '0') {
+    if(key.classList.contains("num")) d = key.id;
+  } else {
+    if(key.classList.contains("num") || key.id === '.') d += key.id;
+  }
+
+  switch (key.id) {
+    case 'AC': {
+      n = 0;
+      d = '0';
+      break;
+    }
+    case '+': {
+      if(!n) {
+        n = d;
+        d += ' + ';
+        op = sum;
+      } else 
+      break;
+    }
+    case '-': {
+      if(!n) {
+        n = d;
+        d += ' - ';
+        op = dif;
+      }
+      break;
+    }
+    case '*': {
+      if(!n) {
+        n = d;
+        d += ' * ';
+        op = mul;
+      }
+      break;
+    }
+    case '/': {
+      if(!n) {
+        n = d;
+        d += ' / ';
+        op = div;
+      }
+      break;
+    }
+    case '^': {
+      if(!n) {
+        n = d;
+        d += ' ^ ';
+        op = pow;
+      }
+      break;
+    }
+    case '+/-': {
+      if(!n) {
+        if(d.charAt(0) === '-') d = d.substring(1);
+        else if(d === '0') d = '-';
+        else  d = '-' + d;
+      } 
+      break;
+    }
+    case '=': {
+      if(op){
+        d = operate(op, n, disp.innerHTML.substring(n.length+2));
+        n = 0;
+        op = 0;
+      }
+      break;
+    }
+  }
+
+  disp.innerHTML = d;
+  
+}
